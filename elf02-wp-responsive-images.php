ded88ae4fd1d810c65e5a1a64f040e31dd66e095
ChrisB <christopher.bohnenberger@gmail.com>
2014-04-28 00:51:32 +0200

    Add Options Page and support for Retina Images

--- elf02-wp-responsive-images.php
+++ elf02-wp-responsive-images.php
@@ -4,35 +4,13 @@
 Plugin Name: elf02 WP Responsive Images
 Plugin URI: http://elf02.de/elf02-wp-responsive-images-wordpress-plugin/
 Description: Responsive image solution using picturefilljs. Original idea by timevko.com.
-Version: 1.2.0
-Author: ChrisB
+Version: 1.2.1
+Author: ChrisB & Martin Wolf
 Author URI: http://elf02.de
 License: MIT
 */
 
 
-    /**
-     * Set your own image sizes and breakpoints.
-     */
-
-    add_theme_support('post-thumbnails');
-    add_image_size('large-img', 1200);
-    add_image_size('medium-img', 700);
-    add_image_size('small-img', 300);
-
-    elf02_wp_responsive_images::$breakpoints = array(
-        '1000' => 'large-img',
-        '600' => 'medium-img',
-        '' => 'small-img'
-    );
-
-
-
-
-
-    /**
-     * Plugin code. Don't touch.
-     */
 
     elf02_wp_responsive_images::get_instance();
 
@@ -47,10 +25,41 @@ License: MIT
             return self::$instance;
         }
 
-        static public $breakpoints = array(
-            '1000' => 'large-img',
-            '600' => 'medium-img',
-            '' => 'small-img'
+        /**
+         * Five default Breakpoints
+         */
+        protected $option_name = 'elf02_wp_responsive_images';
+        protected $option_default = array(
+            'bp1' => array(
+                'name' => 'large-img',
+                'size' => '1100',
+                'size2x' => '1600',
+                'pixel' => '769'
+            ),
+            'bp2' => array(
+                'name' => 'medium-img',
+                'size' => '768',
+                'size2x' => '1536',
+                'pixel' => '321'
+            ),
+            'bp3' => array(
+                'name' => 'small-img',
+                'size' => '320',
+                'size2x' => '640',
+                'pixel' => ''
+            ),
+            'bp4' => array(
+                'name' => '',
+                'size' => '',
+                'size2x' => '',
+                'pixel' => ''
+            ),
+            'bp5' => array(
+                'name' => '',
+                'size' => '',
+                'size2x' => '',
+                'pixel' => ''
+            )
         );
 
         private function __construct() {
@@ -58,9 +67,96 @@ License: MIT
         }
 
         public function init() {
+            // add_image_size from plugin options
+            add_theme_support('post-thumbnails');
+            $options = get_option($this->option_name);
+            $options = ($options === FALSE) ? $this->option_default : $options;
+            foreach ($options as $key => $value) {
+                add_image_size($value['name'], intval($value['size']));
+                if($value['size2x'] !== '') {
+                    $str = $value['name'].'@2x';
+                    add_image_size($str, intval($value['size2x']));
+                }
+            }
+
+            // Set wp actions and filters
             add_action('wp_enqueue_scripts', array($this, 'add_picturefilljs'));
             add_filter('image_send_to_editor', array($this, 'insert_image_with_id'), 10, 9);
             add_filter('the_content', array($this, 'filter_responsive_images'));
+            add_action('admin_init', array($this, 'init_options'));
+            add_action('admin_menu', array($this, 'init_options_page'));
+        }
+
+
+        /**
+         * Options Page handling
+         */
+        public function init_options() {
+            register_setting('responsive_images_options', $this->option_name);
+        }
+
+        public function init_options_page() {
+            add_options_page('Responsive Images Options', 'Responsive Images', 'manage_options', 'responsive_images_options', array($this, 'options_do_page'));
+        }
+
+        public function options_do_page() {
+            $options = get_option($this->option_name);
+            $options = ($options === FALSE) ? $this->option_default : $options;
+            ?>
+            <div class="wrap">
+                <h2>Responsive Images Options</h2>
+                <form method="post" action="options.php">
+                    <?php settings_fields('responsive_images_options'); ?>
+                    <table class="form-table" style="width:500px;">
+                        <tr>
+                            <th>Breakpoint Name</th>
+                            <th>Image Size</th>
+                            <th>Image Size Retina</th>
+                            <th>Breakpoint Pixel (min-width)</th>
+                        </tr>
+                        <?php
+                            for($i=1; $i<=5; $i++) {
+                                echo '<tr>';
+
+                                $str1 = 'bp'.$i;
+                                $str2 = $this->option_name . '['. $str1 .'][name]';
+                                $str3 = $options[$str1]['name'];
+                                printf('<td><input type="text" name="%s" value="%s" /></td>', $str2, $str3);
+
+                                $str2 = $this->option_name . '['. $str1 .'][size]';
+                                $str3 = $options[$str1]['size'];
+                                printf('<td><input type="text" name="%s" value="%s" /></td>', $str2, $str3);
+
+                                $str2 = $this->option_name . '['. $str1 .'][size2x]';
+                                $str3 = $options[$str1]['size2x'];
+                                printf('<td><input type="text" name="%s" value="%s" /></td>', $str2, $str3);
+
+                                $str2 = $this->option_name . '['. $str1 .'][pixel]';
+                                $str3 = $options[$str1]['pixel'];
+                                printf('<td><input type="text" name="%s" value="%s" /></td>', $str2, $str3);
+
+                                echo '</tr>';
+                            }
+                        ?>
+                        <tr>
+                            <td colspan="4">
+                                <ul style="padding:10px;border:2px dashed #1e8cbe;">
+                                    <li><strong>Notes:</strong></li>
+                                    <li><strong>First Breakpoint must be set.</strong> This is also the Fallback Image and should be the largest one.</li>
+                                    <li>"Image Size Retina" is optional and can be blank.</li>
+                                    <li>It is not necessary to set all five Breakpoints.</li>
+                                    <li><strong>Set all values without an additional "px".</strong></li>
+                                    <li><strong>The smallest Breakpoint shouldn't have set a "Breakpoint Pixel" value.</strong></li>
+                                </ul>
+                            </td>
+                        </tr>
+                    </table>
+                    <p class="submit">
+                        <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+                    </p>
+                </form>
+            </div>
+            <?php
         }
 
         /**
@@ -105,16 +201,29 @@ License: MIT
             $markup = '<picture><!--[if IE 9]><video style="display: none;"><![endif]-->';
 
             // Images with media querys and breakpoints
-            foreach(self::$breakpoints as $size => $type)
-            {
-                $imgsrc = wp_get_attachment_image_src($image_id, $type);
-                $media = ($size == '') ? '' : ' media="(min-width:'. $size .'px)"';
-                $arr[] ='<source srcset="'. $imgsrc[0] .'"'. $media .'>';
+            $options = get_option($this->option_name);
+            $options = ($options === FALSE) ? $this->option_default : $options;
+            foreach ($options as $key => $value) {
+
+                if($value['name'] !== '') {
+
+                    $imgsrc = wp_get_attachment_image_src($image_id, $value['name']);
+                    if($value['size2x'] !== '') {
+                        $str = $value['name'].'@2x';
+                        $imgsrc_retina = wp_get_attachment_image_src($image_id, $str);
+                    }
+
+                    $media = ($value['pixel'] === '') ? '' : ' media="(min-width:'. $value['pixel'] .'px)"';
+                    $retina = ($value['size2x'] === '') ? '' : ', '. $imgsrc_retina[0] .' 2x';
+                    $arr[] ='<source srcset="'. $imgsrc[0].$retina .'"'. $media .'>';
+
+                }
             }
+
             $markup .= implode($arr);
 
-            // Fallback image
-            $imgsrc = wp_get_attachment_image_src($image_id, 'large-img');
+            // Fallback Image
+            $imgsrc = wp_get_attachment_image_src($image_id, $options['bp1']['name']);
             $markup .= '<!--[if IE 9]></video><![endif]--><img class="responsive-img" src="'. $imgsrc[0] .'"></picture>';
 
             return $markup;
--- readme.md
+++ readme.md
@@ -1,15 +1,21 @@
 # elf02 WP Responsive Images - WordPress Plugin
 
-**New version with picturefill.js v2**
+Responsive image solution using picturefill.js v2 by [Scott Jehl][1]. Original idea by [Tim Evko][2].
 
-Responsive image solution using picturefill.js by [Scott Jehl][1]. Original idea by [Tim Evko][2].
+## New version 1.2.1
+* Add enhanced code for Retina Images by [Martin Wolf][5]
+* New Options Page for easy control over five Breakpoints.
 
-This version comes without Shortcodes. It extends each posted image with a data-responsive attribute. A "the_content" filter replaced such an image with the picturefill.js markup. After deactivating the plugin or removing the data-responsive attribute, all images should appear as normal.
+## How It Works
+The plugin extends each posted image with a data-responsive attribute. A "the_content" filter replaced such an image with the picturefill.js markup. After deactivating the plugin or removing the data-responsive attribute, all images should appear as normal.
 
-**You can replace the standard image sizes and breakpoints in top of the *elf02-wp-responsive-images.php* file. I'm too lazy to make a settings page ;).**
+## Additional
+To regenerate older images after setting your Breakpoints, you can use the Plugin [Regenerate Thumbnails][4]. Be careful: Not yet tested by me!
 
 Plugin Page: [elf02 WP Responsive Images - WordPress Plugin][3]
 
   [1]: http://scottjehl.github.io/picturefill/
   [2]: https://github.com/tevko/wp-tevko-responsive-images
   [3]: http://elf02.de/elf02-wp-respo…rdpress-plugin/
+  [4]: http://wordpress.org/plugins/regenerate-thumbnails/
+  [5]: http://visuellegedanken.de/
